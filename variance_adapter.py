8d402b95681f078ea86a724dd9efbb17a5b2bae5
Nobi004 <148976917+Nobi004@users.noreply.github.com>
2025-05-21 03:18:35 +0600

    created PitchPredictor function

--- variance_adapter.py
+++ variance_adapter.py
@@ -23,3 +23,18 @@ class DurationPredictor(nn.Module):
         x = self.linear(x.permute(0,2,1)).squeeze(-1)
         return x 
     
+#Pitch Predictor
+class PitchPredictor(nn.Module):
+    def __init__(self,in_dim=256,filter_size=256,kernel_size=3):
+        super().__init__()
+        self.conv1 = nn.Conv1d(in_dim,filter_size,kernel_size,padding=1)
+        self.conv2 = nn.Conv1d(filter_size,filter_size,kernel_size,padding=1)
+        self.linear = nn.Linear(filter_size,1)
+        self.relu = nn.ReLU()
+        
+    def forward(self,x):
+        x = x.permute(0,2,1)
+        x = self.relu(self.conv1(x))
+        x = self.relu(self.conv2(x))
+        self.linear(x.permute(0,2,1)).squeeze(-1)
+        return x
