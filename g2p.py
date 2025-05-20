b3029f06de0358c40e2ee101455d6caa39281a23
Mahmudun Nobi <mdmnb435@gmail.com>
2025-05-20 22:53:51 +0600

    load_cmudict function created

---
+++ g2p.py
@@ -0,0 +1,28 @@
+import numpy as np 
+
+def load_cmudict(file_path):
+    cmudict = {}
+    with open(file_path, 'r') as f:
+        for line in f: 
+            if line.startswith(';;;'): continue
+            parts = line.strip().split('')
+            word = parts[0].lower()
+            phonemes = parts[1:]
+            cmudict[word] = phonemes
+    return cmudict 
+
+# def simple_g2p(text,cmudict):
+#     word = text.lower()
+#     phonemes = cmudict.get(word)
+#     for word in cmudict:
+#         if word.startswith(text):
+#             phonemes.extend(cmudict[word])
+#         else:
+#             phonemes = cmudict.get(word)
+
+#     return phonemes
+
+# cmudict = load_cmudict('cmudict-0.7b')
+# text = "Hello World"
+# phonemes = simple_g2p(text,cmudict)
+# print(phonemes)
