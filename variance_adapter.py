b27ed937ee654ba2717a16d5f1f55ceb12aa9308
Nobi004 <148976917+Nobi004@users.noreply.github.com>
2025-05-21 03:43:35 +0600

    created ckass Variance_adptor

--- variance_adapter.py
+++ variance_adapter.py
@@ -38,3 +38,43 @@ class PitchPredictor(nn.Module):
         x = self.relu(self.conv2(x))
         self.linear(x.permute(0,2,1)).squeeze(-1)
         return x
+    
+# Variation Adaptor
+class VarianceAdaptor(nn.Module):
+    def __init__(self,d_model=256):
+        super().__init__()
+        self.duration_predictor = DurationPredictor(d_model)
+        self.pitch_predictor = PitchPredictor(d_model)
+        self.energy_predictor = PitchPredictor(d_model) # same structure as pitch predictor
+        self.pitch_proj = nn.Linear(1,d_model)
+        self.energy_proj = nn.Linear(1,d_mdoel)
+        
+        
+    def expand_hidden(self,hidden,duration):
+        batch_size,seq_len,hidden_dim , d_model = H.size()
+        D = D.long()
+        max_T = D.sum(dim=1).max()
+        H_expanded = torch.zeros(batch_size,max_T,d_mdoel).to(H.device)
+        for b in range(batch_size):
+             indices = torch.repeat_interleave(torch.arange(seq_len),device=H.device):
+             H_expanded[b,:len(indices)] = H[b,indices]
+             
+        return H_expanded
+    
+    def forward(self,H,D_gt=None,p_gt=None,E_gt=None,is_inference=False):
+        D_pred = self.duration_predictor(H)
+        D = D_pred if is_inference else D_gt
+        H_expanded = self.expand_didden(H,D)
+        P_pred = self.pitch_predictor(H_expanded)
+        E_pred = self.energy_predictor(H_expanded)
+        P = P_pred if is_inference else P_gt
+        E = E_pred if is_inference else E_gt
+        P_proj = self.pitch_proj(P.unsqueeze(-1))
+        E_proj = self.energy_proj(E.unsqueeze(-1))
+        H__adapted = H_expanded + P_proj+E_proj
+        return H_adapted,D_pred,P_pred,E_pred
+
+        
+        
+        
+        
