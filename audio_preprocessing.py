e52c9656e095f319b9884de599ff68f4f35fca27
Nobi004 <148976917+Nobi004@users.noreply.github.com>
2025-05-21 02:46:08 +0600

    created pitch extraction function

--- audio_preprocessing.py
+++ audio_preprocessing.py
@@ -5,3 +5,17 @@ def simple_dtw(audio,phonemes):
     durations = np.array([duration_per_phoneme] *len(phonemes))
     return durations
 
+# pitch extraction function
+def extract_pitch(audio ,sr=22050,frame_length=1024,hop_length=512):
+    frames = [audio[i:i+frame_length] for i in range(0,len(audio)-frame_length,hop_length)]
+    f0 =[]
+    for frame in frames:
+        corr = np.correlate(frame,frame,mode='full')
+        corr = corr[len(corr)//2:]
+        peak = np.argmax(corr[1:]) +1
+        f0.append(sr/peak)
+    return np.array(f0)
+
+
+
+
