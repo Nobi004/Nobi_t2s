c7c2821ab87c1042bb5fab84a8fc918cb9f718dd
Nobi004 <148976917+Nobi004@users.noreply.github.com>
2025-05-21 12:05:17 +0600

    created ResidualBlock class

--- waveform_decoder.py
+++ waveform_decoder.py
@@ -13,6 +13,16 @@ class GateDilatedConv(nn.Module):
         z1,z2 = z.chunk(2.dim=1)
         return torch.tanh(z1)*torch.sigmoid(z2)
     
+class ResidualBlock(nn.Module):
+    def __init__(self,channels,kernel_size,dilation):
+        super().__init__()
+        self.gated_conv = GateDilatedConv(channels,kernel_size,dilation)
+        self.conv1x1 = nn.Conv1d(channels,channels,kernel_size=1)
+        
+    def forward(self,x):
+        out=self.gated_conv(x)
+        residual = self.conv1x1(x)
+        return x + residual
 
 
         
