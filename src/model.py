52202c8ac357eb7ae37916b2aa5eeab7667b2932
Mahmudun Nobi <mdmnb435@gmail.com>
2025-05-21 20:30:16 +0600

    created ResidualBlock class

--- src/model.py
+++ src/model.py
@@ -103,6 +103,18 @@ class GateDialatedConv(nn.Module):
         z1,z2 = z.chunk(2,dim=1)
         return torch.tanh(z1) * torch.sigmoid(z2)
     
+class ResidualBlock(nn.Module):
+    def __init__(self,channels,kernel_size,dilation):
+        super().__init__()
+        self.gated_conv = GateDialatedConv(channels,kernel_size,dilation)
+        self.conv1x1 = nn.Conv1d(channels,channels,kernel_size=1)
+
+    def forward(self,x):
+        out = self.gated_conv(x)
+        residual = self.conv1x1(out)
+        return x + residual
+    
+
 
 
   
