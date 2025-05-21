92b38cf60b53b9edf4521924f31b71cf16c39702
Nobi004 <148976917+Nobi004@users.noreply.github.com>
2025-05-21 12:04:38 +0600

    created GateDilatedConv class

--- waveform_decoder.py
+++ waveform_decoder.py
@@ -1,5 +1,18 @@
 import torch 
 import torch.nn as nn
 
+class GateDilatedConv(nn.Module):
+    def __init__(self,channels,kernel_size,dilation):
+        super().__init__()
+        padding = (kernel_size -1) * dilation // 2 
+        self.conv = nn.Conv1d(channels,2*channels,kernel_size,dilation=dilation,padding=padding)
+        
+        
+    def forward(self,x):
+        z=self.conv(x)
+        z1,z2 = z.chunk(2.dim=1)
+        return torch.tanh(z1)*torch.sigmoid(z2)
+    
+
 
         
