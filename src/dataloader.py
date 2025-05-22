dbf020b226d015c72edbfa84a7fe6053352d498c
Mahmudun Nobi <mdmnb435@gmail.com>
2025-05-22 16:09:56 +0600

    fixes Convert phonemes to indices at dataloader.py

--- src/dataloader.py
+++ src/dataloader.py
@@ -4,8 +4,11 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 
 class TTSDataset(Dataset):
-    def __init__(self, data_dir):
+    def __init__(self, data_dir,max_phoneme_len=100,max_waveform_len=20480,hop_length=256):
         self.data_dir = data_dir
+        self.max_phoneme_len = max_phoneme_len
+        self.max_waveform_len = max_waveform_len
+        self.hop_length = hop_length
         self.audio_ids = [f.split('_')[0] for f in os.listdir(data_dir) if f.endswith('_phonemes.npy')]
         self.phoneme_to_idx = {p: i for i, p in enumerate(set(p for id in self.audio_ids for p in np.load(os.path.join(data_dir, f'{id}_phonemes.npy'), allow_pickle=True)))}
     
@@ -21,7 +24,14 @@ class TTSDataset(Dataset):
         energy = np.load(os.path.join(self.data_dir, f'{audio_id}_energy.npy'))
         
         # Convert phonemes to indices
-        phoneme_indices = torch.tensor([self.phoneme_to_idx[p] for p in phonemes], dtype=torch.long)
+        phoneme_indices = [self.phoneme_to_idx[p] for p in phonemes]
+        phoneme_len = len(phoneme_indices)
+        if phoneme_len > self.max_phoneme_len:
+            phoneme_indices = phoneme_indices[:self.max_phoneme_len]
+            durations = durations[:self.max_phoneme_len]
+        else:
+            phoneme_indices += [self.phoneme_to_idx['<pad>']] * (self.max_phoneme_len - phoneme_len)
+            durations = np.pad(durations, (0, self.max_phoneme_len - phoneme_len), mode='constant') 
         
         # Truncate or pad to fixed length for batching
         max_len = 20480  # ~1 second at 22050 Hz
