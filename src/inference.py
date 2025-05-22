ac87b62cb83d3a8671fc5ec2e4cd4f2f00df1a7d
Mahmudun Nobi <mdmnb435@gmail.com>
2025-05-22 17:25:55 +0600

    some fixes

--- src/inference.py
+++ src/inference.py
@@ -1,2 +1,4 @@
-import torch
-print(torch.cuda.is_available())
+import os
+preprocessed_dir = 'data/preprocessed'
+phoneme_files = [f for f in os.listdir(preprocessed_dir) if f.endswith('_phonemes.npy')]
+print(f"Found {len(phoneme_files)} phoneme files")
